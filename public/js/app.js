e3e31c39b586a57bd40722c8e5f3687aa97bc360
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-06-28 20:01:54 -0400

    added color change

--- public/js/app.js
+++ public/js/app.js
@@ -7,19 +7,21 @@ $(document).ready(function(){
   addClickHandlers();
 
   function changeColor(){
-    console.log('I am changing!!!!!!!')
+    var colorClasses= ['white', 'red', 'green', 'blue'];
+    var colorCycle = Math.round(Math.random()*colorClasses.length)+1;
+    var color = colorClasses[colorCycle];
+    $(this).removeClass(colorClasses.join(' '));
+    $(this).addClass(color);
+
   }
   function addClickHandlers(){
     var cells = $('.cell');
     for(var counter =0; counter<cells.length; counter +=1){
       var cell = cells[counter];
-      $(cell).on('click', changeColor);
+      $(cell).on('mouseenter', changeColor);
     }
 
   }
-
-
-
 
   function initGrid(){
     for(var i = 0; i<numOfRows; i +=1){
